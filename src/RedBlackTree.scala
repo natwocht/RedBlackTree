5e698d600db47c5af432d45c594d25937d6b6109
natwocht <natwocht@gmail.com>
2018-05-27 15:26:08 +0200

    Initial commit

---
+++ src/RedBlackTree.scala
@@ -0,0 +1,16 @@
+import scala.annotation.tailrec
+import scala.math.Ordering.Implicits._
+
+object RedBlackTree {
+
+/*
+* TODO
+* */
+
+
+}
+
+object Run extends App {
+    println("Red-black tree implementation...")
+
+}
