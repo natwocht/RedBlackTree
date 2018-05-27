a81aa767bc0f02b38eabf6fff722946f9eb4780d
natwocht <natwocht@gmail.com>
2018-05-27 17:44:21 +0200

    basic structure

--- src/RedBlackTree.scala
+++ src/RedBlackTree.scala
@@ -3,9 +3,29 @@ import scala.math.Ordering.Implicits._
 
 object RedBlackTree {
 
-/*
-* TODO
-* */
+    object Color extends Enumeration {
+        val Red, Black = Value
+    }
+
+    abstract sealed class Tree[T: Ordering] {
+        def color: Color.Value
+        def value: T
+        def left: Tree[T]
+        def right: Tree[T]
+        def isEmpty: Boolean
+    }
+
+    case class Branch[T: Ordering](color: Color.Value, value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+        def isEmpty = false
+    }
+
+    case object Leaf extends Tree[Nothing] {
+        def color: Color.Value = Color.Black
+        def value: Nothing = throw new NoSuchElementException("An empty tree.")
+        def left: Tree[Nothing] = throw new NoSuchElementException("An empty tree.")
+        def right: Tree[Nothing] = throw new NoSuchElementException("An empty tree.")
+        def isEmpty = true
+    }
 
 
 }
